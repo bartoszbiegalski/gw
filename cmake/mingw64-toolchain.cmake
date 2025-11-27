4e0e832d98714712e96c84e64294e44f133da897
U-LAPTOP-54JVHUSO\User <bartoszbiegalski@example.com>
2025-11-27 10:48:02 +0100

    Makefile toolchain configuration fix

--- cmake/mingw64-toolchain.cmake
+++ cmake/mingw64-toolchain.cmake
@@ -1,16 +1,16 @@
 set(CMAKE_SYSTEM_NAME Windows)
-set(CMAKE_SYSTEM_PROCESSOR x86)
+set(CMAKE_SYSTEM_PROCESSOR x86_64)
 
-set(MINGW_PREFIX "C:/msys64/mingw32")
+set(MINGW_PREFIX "C:/msys64/mingw64")
 
 set(CMAKE_C_COMPILER "${MINGW_PREFIX}/bin/gcc.exe")
 set(CMAKE_CXX_COMPILER "${MINGW_PREFIX}/bin/g++.exe")
 set(CMAKE_RC_COMPILER "${MINGW_PREFIX}/bin/windres.exe")
 
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -m32")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -m32")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -m64")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -m64")
 
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -static -static-libgcc -static-libstdc++ -m32")
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -static -static-libgcc -static-libstdc++ -m64")
 
 set(MSYS_INCLUDE "${MINGW_PREFIX}/include")
 set(MSYS_LIB "${MINGW_PREFIX}/lib")
--- cmake/windows.cmake
+++ cmake/windows.cmake
@@ -1,18 +1,17 @@
-find_package(PkgConfig REQUIRED)
-find_package(ZLIB REQUIRED)
-pkg_check_modules(LIBXML2 REQUIRED libxml-2.0)
-pkg_check_modules(LIBZIP REQUIRED libzip)
-
 target_include_directories(gw_lib PUBLIC
-    ${LIBXML2_INCLUDE_DIRS}
-    ${LIBZIP_INCLUDE_DIRS}
+    ${MSYS_INCLUDE}
+    ${MSYS_INCLUDE}/libxml2
 )
 
 target_link_libraries(gw_lib PUBLIC
-    ${LIBXML2_LIBRARIES}
-    ${LIBZIP_LIBRARIES}
+    ${MSYS_LIB}/libzip.a
+    ${MSYS_LIB}/libxml2.a
     -lz
+    -lbz2
+    -llzma
+    -lzstd
     -liconv
+    -lws2_32
     -lbcrypt
    # cppuprofile
 )
