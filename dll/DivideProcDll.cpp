acd801d185df7d9cacdf29167e51e86ba07b58d2
bartoszbiegalski <147548221+bartoszbiegalski@users.noreply.github.com>
2026-01-28 13:58:43 +0100

    Merging

--- dll/DivideProcDll.cpp
+++ dll/DivideProcDll.cpp
@@ -3,68 +3,107 @@
 #include <windows.h>
 #include <vector>
 #include <string>
+#include <sstream>
+#include <codecvt>
+#include <locale>
 #include "logic/GmlServices.h"
+
+std::string WideToUtf8(const std::wstring &wstr)
+{
+    if (wstr.empty())
+        return {};
+    int len = WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), -1, nullptr, 0, nullptr, nullptr);
+    std::string out(len - 1, '\0');
+    WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), -1, out.data(), len, nullptr, nullptr);
+    return out;
+}
+
+typedef void(__stdcall *LogCallback)(const wchar_t *msg);
+
+static LogCallback g_log = nullptr;
 
 extern "C"
 {
-    /// UTF-16 callback
-    typedef void(_stdcall *TProgressW)(int percent, const wchar_t *message, void *user);
-    /// ANSI callback
-    typedef void(_stdcall *TProgressA)(int percent, const char *message, void *user);
+    __declspec(dllexport) void __stdcall SetLogCallback(LogCallback cb)
+    {
+        g_log = cb;
+    }
 
-    __declspec(dllexport) int __stdcall DivideGmlW(const wchar_t *inFile,
-                                                   TProgressW progress,
-                                                   void *userData)
+    void Log(const std::wstring &msg)
+    {
+        if (g_log)
+            g_log(msg.c_str());
+    }
+}
+
+extern "C"
+{
+    __declspec(dllexport) int __stdcall DivideGmlW(const wchar_t *inFile, const wchar_t *params)
     {
         if (!inFile)
             return -1;
 
         std::vector<std::string> nsVect;
-
-        if (userData)
+        bool isZip = false;
+        if (params)
         {
-            const wchar_t **args = static_cast<const wchar_t **>(userData);
-            for (int i = 0; args[i] != nullptr; ++i)
+            std::wstring wstr(params);
+            std::wstring_convert<std::codecvt_utf8<wchar_t>> conv;
+            std::string utf8str = conv.to_bytes(wstr);
+            std::istringstream iss(utf8str);
+            std::string token;
+            while (iss >> token)
             {
-                std::wstring ws(args[i]);
-                nsVect.push_back(NamespacePrefix{std::string(ws.begin(), ws.end())});
+                if (token == "-z" || token == "-Z")
+                {
+                    isZip = true;
+                }
+                else
+                {
+                    nsVect.push_back(NamespacePrefix{token});
+                }
             }
         }
-
-        std::wstring wsIn(inFile);
-        std::string fileIn(wsIn.begin(), wsIn.end());
-        wchar_t msg[128];
-        int i = 100;
-        wsprintfW(msg, L"Cos tam robie na %d procent", i);
-        progress(i, msg, userData);
-        // GmlServices::PerformDivision(fileIn, nsVect);
+        std::string inFileU8 = WideToUtf8(inFile);
+        std::filesystem::path filePath(inFileU8);
+        auto cfg = std::make_unique<XmlConfig>(static_config::staticData);
+        try
+        {
+            GmlServices::PerformDivision(filePath, nsVect, isZip);
+        }
+        catch (...)
+        {
+            Log(L"exception");
+        }
         return 0;
     }
 
-    __declspec(dllexport) int __stdcall DivideGmlA(const char *inFile,
-                                                   TProgressA progress,
-                                                   void *userData)
+    __declspec(dllexport) int __stdcall DivideGmlA(const char *inFile, void *params)
     {
         if (!inFile)
             return -1;
 
         std::vector<std::string> nsVect;
-
-        if (userData)
+        bool isZip = false;
+        if (params)
         {
-            const char **args = static_cast<const char **>(userData);
+            const char **args = static_cast<const char **>(params);
             for (int i = 0; args[i] != nullptr; ++i)
             {
-                nsVect.push_back(NamespacePrefix{std::string(args[i])});
+                std::string argStr(args[i]);
+                if (argStr == "-Z" || argStr == "-z")
+                {
+                    isZip = true;
+                }
+                else
+                {
+                    nsVect.push_back(NamespacePrefix{argStr});
+                }
             }
         }
 
         std::string fileIn(inFile);
-        char msg[128];
-        int i = 100;
-        wsprintfA(msg, "Przetwarzanie... %d%%", i);
-        progress(i, msg, userData);
-        // GmlServices::PerformDivision(fileIn, nsVect);
+        // GmlServices::PerformDivision(fileIn, nsVect, isZip);
         return 0;
     }
 }
