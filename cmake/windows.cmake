e5e2acc4349a02e7f059063e1c766d3562d438a8
bartoszbiegalski <147548221+bartoszbiegalski@users.noreply.github.com>
2026-08-19 11:38:11 +0200

    Revert cmake/windows.cmake and CMakeLists to 49975c83e367f27fe62a058ef29b2bf5ac116510

--- cmake/windows.cmake
+++ cmake/windows.cmake
@@ -1,10 +1,10 @@
-# set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -static -static-libgcc -static-libstdc++")
-# set(CMAKE_FIND_LIBRARY_SUFFIXES ".a")
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -static -static-libgcc -static-libstdc++")
+set(CMAKE_FIND_LIBRARY_SUFFIXES ".a")
 
-# set(LIBXML2_LIBRARY "C:/msys64/mingw64/lib/libxml2.a" CACHE FILEPATH "" FORCE)
-# set(LIBXML2_INCLUDE_DIR "C:/msys64/mingw64/include/libxml2" CACHE PATH "" FORCE)
-# set(LibXml2_DIR "C:/msys64/mingw64/lib/cmake/libxml2" CACHE PATH "" FORCE)
-# set(LibXml2_USE_STATIC_LIBS ON CACHE BOOL "" FORCE)
+set(LIBXML2_LIBRARY "C:/msys64/mingw64/lib/libxml2.a" CACHE FILEPATH "" FORCE)
+set(LIBXML2_INCLUDE_DIR "C:/msys64/mingw64/include/libxml2" CACHE PATH "" FORCE)
+set(LibXml2_DIR "C:/msys64/mingw64/lib/cmake/libxml2" CACHE PATH "" FORCE)
+set(LibXml2_USE_STATIC_LIBS ON CACHE BOOL "" FORCE)
 
 set(LIBZIP_LIBRARY "C:/msys64/mingw64/lib/libzip.a" CACHE FILEPATH "" FORCE)
 set(LIBZIP_INCLUDE_DIR "C:/msys64/mingw64/include" CACHE PATH "" FORCE)
@@ -15,15 +15,14 @@ set(GEOS_LIBRARY "C:/msys64/mingw64/lib/libgeos.a" CACHE FILEPATH "" FORCE)
 set(GEOS_INCLUDE_DIR "C:/msys64/mingw64/include/geos" CACHE PATH "" FORCE)
 set(GEOS_USE_STATIC_LIBS ON CACHE BOOL "" FORCE)
 
-find_package(LibXml2 CONFIG REQUIRED)
-#find_package(LibZip CONFIG REQUIRED)
-find_package(GEOS CONFIG REQUIRED)
+find_package(LibXml2 REQUIRED ON)
+find_package(LibZip REQUIRED ON)
+find_package(GEOS REQUIRED ON)
 
 # gw_lib
 add_library(gw_lib STATIC ${GW_LIBRARY_SOURCES})
-
-# target_compile_definitions(gw_lib PUBLIC ZIP_STATIC)
-# target_compile_definitions(gw_lib PUBLIC LIBXML_STATIC)
+target_compile_definitions(gw_lib PUBLIC ZIP_STATIC)
+target_compile_definitions(gw_lib PUBLIC LIBXML_STATIC)
 
 target_include_directories(gw_lib PUBLIC
     ${CMAKE_CURRENT_SOURCE_DIR}/include
@@ -31,6 +30,7 @@ target_include_directories(gw_lib PUBLIC
     ${LIBZIP_INCLUDE_DIR}
     ${GEOS_INCLUDE_DIR}
 )
+
 target_link_libraries(gw_lib PUBLIC
     LibXml2::LibXml2
     ${LIBZIP_LIBRARY}
@@ -44,23 +44,18 @@ target_link_libraries(gw_lib PUBLIC
     -lbcrypt
 )
 
-# # gw_dll
-# add_library(gw_dll SHARED ${DLL_SOURCES})
-# target_link_options(gw_dll PRIVATE
-#     # -static
-#     # -static-libstdc++
-#     # -static-libgcc
-# )
-# target_include_directories(gw_dll PRIVATE 
-#     ${CMAKE_CURRENT_SOURCE_DIR}/include
 
-# )
-# target_link_libraries(gw_dll PRIVATE
-#     gw_lib
-# )
+# gw_dll
+add_library(gw_dll SHARED ${DLL_SOURCES})
+target_link_options(gw_dll PRIVATE
+    -static
+    -static-libstdc++
+    -static-libgcc
+)
+target_include_directories(gw_dll PRIVATE 
+    ${CMAKE_CURRENT_SOURCE_DIR}/include
 
-# set_target_properties(gw_dll PROPERTIES
-#   CXX_STANDARD 17
-#   CXX_STANDARD_REQUIRED YES
-#   CXX_EXTENSIONS NO
-# )
+)
+target_link_libraries(gw_dll PRIVATE
+    gw_lib
+)
